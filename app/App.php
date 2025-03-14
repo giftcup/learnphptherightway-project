cb8b14032197176179fdb9cd5acac4a446bb1cf5
giftcup <salometambe@gmail.com>
2025-03-14 07:07:46 +0100

    Read transaction files and transaction data

--- app/App.php
+++ app/App.php
@@ -2,4 +2,34 @@
 
 declare(strict_types = 1);
 
-// Your Code
+function get_transaction_files(string $dir_path): array {
+    $files = [];
+
+    foreach(scandir($dir_path) as $file) {
+        if(is_dir($file)) {
+            continue;
+        }
+        array_push($files, $dir_path . $file);
+    }
+
+    var_dump($files);
+    return $files;
+}
+
+function get_transaction(string $filename): array{
+    if (! file_exists($filename)) {
+        trigger_error("File $filename does not exist", E_USER_ERROR);
+    }
+
+    $file = fopen($filename, 'r');
+
+    $transactions = [];
+
+    while(($transaction = fgetcsv($file)) !== false) {
+        $transactions[] = $transaction;
+    }
+
+    print_r($transactions);
+
+    return $transactions;
+}
--- public/index.php
+++ public/index.php
@@ -9,3 +9,10 @@ define('FILES_PATH', $root . 'transaction_files' . DIRECTORY_SEPARATOR);
 define('VIEWS_PATH', $root . 'views' . DIRECTORY_SEPARATOR);
 
 /* YOUR CODE (Instructions in README.md) */
+require APP_PATH . "App.php";
+
+$files = get_transaction_files(FILES_PATH);
+
+foreach ($files as $file) {
+    get_transaction($file);
+}
