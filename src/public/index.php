22e6f78ad73e47289df23a1e64effd7666c17083
giftcup <salometambe@gmail.com>
2025-04-03 11:53:10 +0100

    Autoloading with composer

--- src/public/index.php
+++ src/public/index.php
@@ -2,23 +2,14 @@
 
 declare(strict_types=1);
 
-// require_once '../app/PaymentGateway/Stripe/Transaction.php';
-// require_once '../app/PaymentGateway/Paddle/Transaction.php';
-// require_once '../app/PaymentGateway/Paddle/CustomerProfile.php';
-// require_once '../app/Notification/Email.php';
-
-spl_autoload_register(function($class) {
-    $path = __DIR__ . '/../' . lcfirst(str_replace('\\', '/', $class)) . '.php';
-    
-    if(file_exists($path)) {
-        require $path;
-    }
-});
+require __DIR__ . '/../vendor/autoload.php';
 
 use App\PaymentGateway\Paddle\Transaction;
-use App\PaymentGateway\Stripe\Transaction as StripeTransaction;
 
 $paddleTransaction = new Transaction();
-$stripeTransaction = new StripeTransaction();
 
-var_dump($paddleTransaction, $stripeTransaction);
+$id = new \Ramsey\Uuid\UuidFactory();
+
+echo $id->uuid4();
+
+var_dump($paddleTransaction);
