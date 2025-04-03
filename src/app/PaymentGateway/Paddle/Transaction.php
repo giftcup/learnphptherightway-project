c2851ed14ad8aee613dcda9387a6b77377914231
giftcup <salometambe@gmail.com>
2025-04-03 17:16:17 +0100

    Using statics and constants

--- src/app/PaymentGateway/Paddle/Transaction.php
+++ src/app/PaymentGateway/Paddle/Transaction.php
@@ -4,9 +4,14 @@ declare(strict_types=1);
 
 namespace App\PaymentGateway\Paddle;
 
-class Transaction {
+class Transaction 
+{
+   const STATUS_PAID = 'paid';
+   const STATUS_PENDING = 'status_pending';
+   const STATUS_DECLINED = 'declined';
+
      public function __construct()
      {
-        var_dump(new CustomerProfile());
+        var_dump(self::STATUS_DECLINED);
      }
 }
