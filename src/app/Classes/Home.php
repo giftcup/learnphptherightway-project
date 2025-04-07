6de344840af746bde00a6247e3234029ca51b22f
giftcup <salometambe@gmail.com>
2025-04-07 17:30:01 +0100

    File uploads, sessions, and cookies

--- src/app/Classes/Home.php
+++ src/app/Classes/Home.php
@@ -6,6 +6,31 @@ namespace App\Classes;
 
 class Home {
     public function index(): string {
-        return 'Home';
+        setcookie(
+            'userName',
+            'Gio',
+            time() + (24 * 60 * 60)
+        );
+
+        return <<<FORM
+            <form action='/upload' method='post' enctype='multipart/form-data'>
+                <input type='file' name='receipt' />
+                <button type='submit'>Upload</button>
+            </form>
+        FORM;
+    }
+
+    public function upload() {
+        echo '<pre>';
+        var_dump($_FILES);
+        echo '</pre>';
+
+        $filePath = STORAGE_PATH . '/' . $_FILES['receipt']['name'];
+
+        move_uploaded_file($_FILES['receipt']['tmp_name'], $filePath);
+
+        echo '<pre>';
+        var_dump(pathinfo($filePath));
+        echo '</pre>';
     }
 }
--- src/public/index.php
+++ src/public/index.php
@@ -4,10 +4,18 @@ declare(strict_types=1);
 
 require __DIR__ . '/../vendor/autoload.php';
 
+session_start();
+
+define('STORAGE_PATH', __DIR__ . '/../storage');
+
 $router = new App\Router();
 
-$router->register('/', [App\Classes\Home::class, 'index'])
-    ->register('/invoices', [App\Classes\Invoice::class, 'index'])
-    ->register('/invoices/create', [App\Classes\Invoice::class, 'create']);
+$router->get('/', [App\Classes\Home::class, 'index'])
+    ->post('/upload', [App\Classes\Home::class, 'upload'])
+    ->get('/invoices', [App\Classes\Invoice::class, 'index'])
+    ->get('/invoices/create', [App\Classes\Invoice::class, 'create']);
 
-echo $router->resolve($_SERVER['REQUEST_URI']);
+echo $router->resolve(
+    $_SERVER['REQUEST_URI'], 
+    strtolower($_SERVER['REQUEST_METHOD'])
+);
